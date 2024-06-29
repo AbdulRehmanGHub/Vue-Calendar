32fb4a7c13d7ba719e54610c66f3e9a36ea7f9a1
Abdul Rehman <pakkhan9009@gmail.com>
2024-06-30 01:35:07 +0500

    files added

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/Vue-Calendar/',
   transpileDependencies: true
 });
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'prdoduction' ? '/Vue-Calendar/' : '/'
-};
